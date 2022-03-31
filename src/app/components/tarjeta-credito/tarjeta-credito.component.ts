2c04f139b350ff66aeb16629f23982431f1a6077
Stuard Quiroz <stuuard96@gmail.com>
2022-03-31 16:36:40 -0500

    Terminamos las validaciones

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -10,9 +11,29 @@ export class TarjetaCreditoComponent implements OnInit {
     {titular: 'Juan Perez',  numeroTarjeta: '2532423432432', fechaExpiracion: '11/12', cvv:'123'},
     {titular: 'Ana Silva',  numeroTarjeta: '1231231232432', fechaExpiracion: '12/12', cvv:'321'},
   ];
-  constructor() { }
+
+  form: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.form=this.fb.group({
+      titular:['', Validators.required],
+      numeroTarjeta:['', [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      fechaExpiracion:['', [Validators.required, Validators.maxLength(5), Validators.minLength(5)]],
+      cvv:['', [Validators.required, Validators.maxLength(3), Validators.minLength(3)]],
+    });
+   }
 
   ngOnInit(): void {
   }
 
+  agregarTarjeta(){
+    const tarjeta: any={
+      titular: this.form.get('titular')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+      cvv: this.form.get('cvv')?.value,
+    }
+    this.listTarjetas.unshift(tarjeta);
+    this.form.reset();
+  }
 }
