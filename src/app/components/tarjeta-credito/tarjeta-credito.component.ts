10c50c3246483cf14374a38f31e12f02807a4f82
Stuard Quiroz <stuuard96@gmail.com>
2022-03-31 15:43:27 -0500

    Se termino la parte front

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
-
+  listTarjetas: any[]=[
+    {titular: 'Juan Perez',  numeroTarjeta: '2532423432432', fechaExpiracion: '11/12', cvv:'123'},
+    {titular: 'Ana Silva',  numeroTarjeta: '1231231232432', fechaExpiracion: '12/12', cvv:'321'},
+  ];
   constructor() { }
 
   ngOnInit(): void {
